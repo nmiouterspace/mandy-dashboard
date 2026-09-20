a1befb251f69aabb76b78691bfd67277b1ed81ed
nmiouterspace <nmi.outerspace@gmail.com>
2026-09-20 16:12:51 +0700

    Restore add student save label

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "mandy-dashboard-v20260920-mobilefix1";
+const CACHE_NAME = "mandy-dashboard-v20260920-mobilefix2";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260920-mobilefix1",
-  "./script.js?v=20260920-mobilefix1",
+  "./styles.css?v=20260920-mobilefix2",
+  "./script.js?v=20260920-mobilefix2",
   "./manifest.webmanifest",
   "./assets/mandy-logo.png",
   "./assets/payment-qr.png"
