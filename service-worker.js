622ca74f1e6bee44f5b09479e221c094b9f4daa0
nmiouterspace <nmi.outerspace@gmail.com>
2026-09-24 12:01:51 +0700

    Add quick Drive load button

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "mandy-dashboard-v20260922-studentsfit2";
+const CACHE_NAME = "mandy-dashboard-v20260924-quickload1";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260922-studentsfit2",
-  "./script.js?v=20260922-studentsfit2",
+  "./styles.css?v=20260924-quickload1",
+  "./script.js?v=20260924-quickload1",
   "./manifest.webmanifest",
   "./assets/mandy-logo.png",
   "./assets/payment-qr.png"
